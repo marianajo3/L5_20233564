05c20ea968e43926cb3ce127d77c358269e3ab12
Mariana <mariana@example.com>
2026-05-07 19:49:46 -0500

    primer commit

---
+++ src/main/java/com/example/l5_20233564/L520233564Application.java
@@ -0,0 +1,13 @@
+package com.example.l5_20233564;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L520233564Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L520233564Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/l5_20233564/L520233564ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l5_20233564;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L520233564ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
